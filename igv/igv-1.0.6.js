8cbd85e03933ef1d6d8acac93971e23c0e6a5187
tmtabor <tabor@broadinstitute.org>
2017-02-10 15:44:05 -0800

    * Update to igv.js 1.0.6 * Revert back to IGV's old base LSID

--- igv/igv-1.0.6.js
+++ igv/igv-1.0.6.js
@@ -1192,7 +1192,7 @@ var igv = (function (igv) {
         });
 
 
-        function decodeBamRecords(ba, offset, alignments, min, max, chrId, filter) {
+        function decodeBamRecords(ba, offset, alignmentContainer, min, max, chrId, filter) {
 
             var blockSize,
                 blockEnd,
@@ -1234,8 +1234,15 @@ var igv = (function (igv) {
                 refID = readInt(ba, offset + 4);
                 pos = readInt(ba, offset + 8);
 
-                if (refID > chrId || pos > max) return;  // We've gone off the right edge => we're done
-                else if (refID < chrId) continue;    // Not sure this is possible
+                if(refID < 0) {
+                    return;   // unmapped reads
+                }
+                else if (refID > chrId || pos > max) {
+                    return;    // off right edge, we're done
+                }
+                else if (refID < chrId) {
+                    continue;   // to left of start, not sure this is possible
+                }
 
                 bmn = readInt(ba, offset + 12);
                 bin = (bmn & 0xffff0000) >> 16;
@@ -1333,7 +1340,7 @@ var igv = (function (igv) {
                         blocks = makeBlocks(alignment, cigarArray);
                         alignment.blocks = blocks.blocks;
                         alignment.insertions = blocks.insertions;
-                        alignments.push(alignment);
+                        alignmentContainer.push(alignment);
                     }
                 }
                 offset = blockEnd;
@@ -4459,18 +4466,22 @@ var igv = (function (igv) {
 
     igv.Browser.prototype.loadTracksWithConfigList = function (configList) {
 
-        var self = this;
+        var self = this,
+            loadedTracks = [];
+
 
         configList.forEach(function (config) {
-            self.loadTrack(config);
+            loadedTracks.push(self.loadTrack(config));
         });
 
         // Really we should just resize the new trackViews, but currently there is no way to get a handle on those
         this.trackViews.forEach(function (trackView) {
             trackView.resize();
-        })
-
+        });
+        
+        return loadedTracks;
     };
+
 
     igv.Browser.prototype.loadTrack = function (config) {
 
@@ -4555,6 +4566,8 @@ var igv = (function (igv) {
         else {
             self.addTrack(newTrack);
         }
+
+        return newTrack;
 
     };
 
@@ -4903,19 +4916,19 @@ var igv = (function (igv) {
         console.log('browser.zoomIn - src extent ' + basesExtent(this.trackViewportWidth(), this.referenceFrame.bpPerPixel));
 
         // Have we reached the zoom-in threshold yet? If so, bail.
-        if (this.minimumBasesExtent() > basesExtent(this.trackViewportWidth(), this.referenceFrame.bpPerPixel/2.0)) {
-            console.log('browser.zoomIn - dst extent ' + basesExtent(this.trackViewportWidth(), this.referenceFrame.bpPerPixel/2.0) + ' bailing ...');
+        if (this.minimumBasesExtent() > basesExtent(this.trackViewportWidth(), this.referenceFrame.bpPerPixel / 2.0)) {
+            console.log('browser.zoomIn - dst extent ' + basesExtent(this.trackViewportWidth(), this.referenceFrame.bpPerPixel / 2.0) + ' bailing ...');
             return;
         } else {
-            console.log('browser.zoomIn - dst extent ' + basesExtent(this.trackViewportWidth(), this.referenceFrame.bpPerPixel/2.0));
+            console.log('browser.zoomIn - dst extent ' + basesExtent(this.trackViewportWidth(), this.referenceFrame.bpPerPixel / 2.0));
         }
 
         // window center (base-pair units)
-        centerBP = this.referenceFrame.start + this.referenceFrame.bpPerPixel * (this.trackViewportWidth()/2);
+        centerBP = this.referenceFrame.start + this.referenceFrame.bpPerPixel * (this.trackViewportWidth() / 2);
 
         // derive scaled (zoomed in) start location (base-pair units) by multiplying half-width by halve'd bases-per-pixel
         // which results in base-pair units
-        this.referenceFrame.start = centerBP - (this.trackViewportWidth()/2) * (this.referenceFrame.bpPerPixel/2.0);
+        this.referenceFrame.start = centerBP - (this.trackViewportWidth() / 2) * (this.referenceFrame.bpPerPixel / 2.0);
 
         // halve the bases-per-pixel
         this.referenceFrame.bpPerPixel /= 2.0;
@@ -4986,7 +4999,7 @@ var igv = (function (igv) {
 
         if (isLocusFeature(feature, this.genome, force)) {
 
-            var success =  gotoLocusFeature(feature, this.genome, this);
+            var success = gotoLocusFeature(feature, this.genome, this);
 
             if ((force || true === success) && callback) {
                 callback();
@@ -5025,7 +5038,7 @@ var igv = (function (igv) {
                         //alert('No feature found with name "' + feature + '"');
                         igv.presentAlert('No feature found with name "' + feature + '"');
                     }
-                    else{
+                    else {
 
                         // Just take the first result for now
                         // TODO - merge results, or ask user to choose
@@ -5075,7 +5088,7 @@ var igv = (function (igv) {
 
         type = 'locus';
         tokens = locusFeature.split(":");
-        chrName = genome.getChromosomeName(tokens[ 0 ]);
+        chrName = genome.getChromosomeName(tokens[0]);
         if (chrName) {
             chr = genome.getChromosome(chrName);
         }
@@ -5088,10 +5101,10 @@ var igv = (function (igv) {
                 start = 0;
                 end = chr.bpLength;
             } else {
-                startEnd = tokens[ 1 ].split("-");
-                start = Math.max(0, parseInt(startEnd[ 0 ].replace(/,/g, "")) - 1);
+                startEnd = tokens[1].split("-");
+                start = Math.max(0, parseInt(startEnd[0].replace(/,/g, "")) - 1);
                 if (2 === startEnd.length) {
-                    end = Math.min(chr.bpLength, parseInt(startEnd[ 1 ].replace(/,/g, "")));
+                    end = Math.min(chr.bpLength, parseInt(startEnd[1].replace(/,/g, "")));
                     if (end < 0) {
                         // This can happen from integer overflow
                         end = chr.bpLength;
@@ -5099,7 +5112,7 @@ var igv = (function (igv) {
                 }
             }
 
-            obj = { start: start, end: end };
+            obj = {start: start, end: end};
             validateLocusExtent(igv.browser, chr, obj);
             start = obj.start;
             end = obj.end;
@@ -5122,7 +5135,7 @@ var igv = (function (igv) {
 
             if (undefined === ee) {
 
-                ss -= igv.browser.minimumBasesExtent()/2;
+                ss -= igv.browser.minimumBasesExtent() / 2;
                 ee = ss + igv.browser.minimumBasesExtent();
 
                 if (ee > chromosome.bpLength) {
@@ -5135,15 +5148,15 @@ var igv = (function (igv) {
 
             } else if (ee - ss < igv.browser.minimumBasesExtent()) {
 
-                center = (ee + ss)/2;
-                if (center - igv.browser.minimumBasesExtent()/2 < 0) {
+                center = (ee + ss) / 2;
+                if (center - igv.browser.minimumBasesExtent() / 2 < 0) {
                     ss = 0;
                     ee = ss + igv.browser.minimumBasesExtent();
-                } else if (center + igv.browser.minimumBasesExtent()/2 > chromosome.bpLength) {
+                } else if (center + igv.browser.minimumBasesExtent() / 2 > chromosome.bpLength) {
                     ee = chromosome.bpLength;
                     ss = ee - igv.browser.minimumBasesExtent();
                 } else {
-                    ss = center - igv.browser.minimumBasesExtent()/2;
+                    ss = center - igv.browser.minimumBasesExtent() / 2;
                     ee = ss + igv.browser.minimumBasesExtent();
                 }
             }
@@ -5298,7 +5311,7 @@ var igv = (function (igv) {
             _left = Math.max(50, xy.x - 5);
 
             _left = Math.min(igv.browser.trackContainerDiv.clientWidth - 65, _left);
-            $element.css({ left: _left + 'px' });
+            $element.css({left: _left + 'px'});
         });
 
 
@@ -7985,8 +7998,8 @@ var igv = (function (igv) {
                 chr: tokens[2],
                 start: parseInt(tokens[4]),
                 end: parseInt(tokens[5]),
-                id: tokens[0],
-                name: tokens[1],
+                id: tokens[1],
+                name: tokens[0],
                 strand: tokens[3],
                 cdStart: parseInt(tokens[6]),
                 cdEnd: parseInt(tokens[7])
